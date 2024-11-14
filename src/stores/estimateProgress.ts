0c7d1aa566047ba7e7a02c4778c111291d099a63
alorslouis <alorslouis@users.noreply.github.com>
2024-11-14 12:52:54 -0500

    init store

--- src/stores/estimateProgress.ts
+++ src/stores/estimateProgress.ts
@@ -1,4 +1,10 @@
 import { atom } from "nanostores";
 import { z } from "zod"
 
-export const estimateStore = atom({})
+export const estimateParser = z.object({
+	zipCode: z.string().length(5)
+})
+
+export type EstimateStoreType = z.infer<typeof estimateParser>
+
+export const estimateStore = atom<EstimateStoreType>({ zipCode: "" })
