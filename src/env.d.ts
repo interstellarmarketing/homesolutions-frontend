13beb91202327a97f69f0f76f665e9237c281ed8
alorslouis <alorslouis@users.noreply.github.com>
2024-11-15 11:00:58 -0500

    wrangler types

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="../.astro/types.d.ts" />
 type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
+type KVNamespace = import("@cloudflare/workers-types").KVNamespace;
 
 declare namespace App {
 	interface Locals extends Runtime { }
