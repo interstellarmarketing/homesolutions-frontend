c01005994f098ff17fd81ba69c719d75105013b2
alorslouis <alorslouis@users.noreply.github.com>
2024-11-15 11:00:44 -0500

    wip

--- src/stores/estimateProgress.ts
+++ src/stores/estimateProgress.ts
@@ -2,9 +2,18 @@ import { atom } from "nanostores";
 import { z } from "zod"
 
 export const estimateParser = z.object({
-	zipCode: z.string().length(5)
+	streetAddress: z.string().min(5),
+	city: z.string().min(2),
+	state: z.string().length(2),
+	zipCode: z.string().length(5),
+	firstName: z.string().min(2),
+	lastName: z.string().min(2),
+	email: z.string().email(),
+	phone: z.number(),
+	isHomeowner: z.boolean(),
+	px: z.enum(["f"])
 })
 
 export type EstimateStoreType = z.infer<typeof estimateParser>
 
-export const estimateStore = atom<EstimateStoreType>({ zipCode: "" })
+export const estimateStore = atom<EstimateStoreType>({ zipCode: "", firstName: "", lastName: "", state: "", streetAddress: "", city: "", isHomeowner: true, email: "", phone: 0 })
