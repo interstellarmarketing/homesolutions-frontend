27fb5d9fdee557f094499f621d75d277b545e81c
alorslouis <alorslouis@users.noreply.github.com>
2024-11-20 14:22:15 -0500

    wip stashedResultId store

--- src/stores/stashedResultId.ts
+++ src/stores/stashedResultId.ts
@@ -1,4 +1,9 @@
 import { atom } from "nanostores";
 import { z } from "zod"
+import { resultIdParser, type ResultId } from "@utils/kv/storedResults"
 
-export const estimateStore = atom<EstimateStoreTypeLoose>({})
+const resultIdPartial = resultIdParser.partial()
+
+export type ResultIdPartial = z.infer<typeof resultIdPartial>
+
+export const resultIdStore = atom<ResultIdPartial>({})
