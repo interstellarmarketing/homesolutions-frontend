c8f25a997123645bba4dbd0ecd354e5097eb71fa
alorslouis <alorslouis@users.noreply.github.com>
2024-11-22 16:51:43 -0500

    mfing PoS
    fixed incorrect type inference
    
    reference types=@cloudflare/workers-types :(

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+/// <reference types="@cloudflare/workers-types" />
+
 type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
-type KVNamespace = import("@cloudflare/workers-types").KVNamespace;
-//type BACKEND = import("estimates-backend-rpc/src/index").ZipStore
+//type KVNamespace = import("@cloudflare/workers-types").KVNamespace;
+
+//import type { KVNamespace } from "@cloudflare/workers-types";
+//type Service = import("@cloudflare/workers-types").Service;
+//type BACKEND = import("@cloudflare/workers-types").Service<import("estimates-backend-rpc/src/index").ZipStore>
 
 declare namespace App {
 	interface Locals extends Runtime { }
