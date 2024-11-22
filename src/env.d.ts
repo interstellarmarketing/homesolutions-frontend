58c039b9a30d9b23e908c075b930cc651617cf42
alorslouis <alorslouis@users.noreply.github.com>
2024-11-22 15:15:53 -0500

    env

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 type KVNamespace = import("@cloudflare/workers-types").KVNamespace;
+//type BACKEND = import("estimates-backend-rpc/src/index").ZipStore
 
 declare namespace App {
 	interface Locals extends Runtime { }
