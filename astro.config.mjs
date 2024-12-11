9349050e6818b1576407c768f7bc01d47b6f5b48
alorslouis <alorslouis@users.noreply.github.com>
2024-12-10 21:29:10 -0800

    bump: astro v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  experimental: {
-    directRenderScript: true,
-  },
   security: {
     checkOrigin: true,
   },
