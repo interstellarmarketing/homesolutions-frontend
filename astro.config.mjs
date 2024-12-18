4427cd86376ebaccc7d425f003d013ec8f02a72b
alorslouis <alorslouis@users.noreply.github.com>
2024-12-17 19:20:03 -0800

    add icon packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 import icon from "astro-icon";
-
+//
 // https://astro.build/config
 export default defineConfig({
   output: "server",
