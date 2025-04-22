78958d709302a41d9791e901b7af6bbd85ed6bdc
Ian Harvey <ian.harvey@toptal.com>
2025-04-22 13:39:08 -0400

    fixes

--- src/stores/estimateProgress.ts
+++ src/stores/estimateProgress.ts
@@ -1,4 +1,3 @@
-import type { EstimateStoreType } from '@models/estimateProgress';
 import type { PublicLeadsUpdateSchema } from '@models/supabase/zodTypes';
 import { persistentAtom } from '@nanostores/persistent';
 import { supabase } from '@utils/supabase';
@@ -13,17 +12,53 @@ export const estimateProgressStore = persistentAtom<PublicLeadsUpdateSchema>(
   }
 );
 
-export type TrackingParams = EstimateStoreType["trackingParams"];
 
-
-export async function resetEstimateProgressFields() {
+export async function resetEstimateProgressFields(ipAddress?: string | null) {
   try {
+    estimateProgressStore.set({});
     const posthogPersonId = posthog.get_distinct_id();
+
+    const userAgent = window.navigator.userAgent;
+    let fbp: string | undefined;
+    let fbc: string | undefined;
+
+    const searchParams = new URLSearchParams(window.location.search);
+
+    const trackingSearchParams = {
+      utm_source: searchParams.get("utm_source") || undefined,
+      utm_medium: searchParams.get("utm_medium") || undefined,
+      utm_campaign: searchParams.get("utm_campaign") || undefined,
+      utm_content: searchParams.get("utm_content") || undefined,
+      utm_term: searchParams.get("utm_term") || undefined,
+      fbclid: searchParams.get("fbclid") || undefined,
+      gclid: searchParams.get("gclid") || undefined,
+      wbraid: searchParams.get("wbraid") || undefined,
+      gbraid: searchParams.get("gbraid") || undefined,
+      ssn: searchParams.get("ssn") || undefined,
+    };
+
+    try {
+      fbp = document.body.getAttribute("data-fbp") || undefined;
+      fbc = document.body.getAttribute("data-fbc") || undefined;
+    } catch (e) {
+      console.error("Error getting facebook cookie", e);
+    }
+
     const { data, error } = await supabase
       .from("leads")
       .insert({
         status: "new",
         posthog_person_id: posthogPersonId,
+        landing_page: window.location.href.split("/").slice(0, -1).join("/"),
+        ip_address: ipAddress,
+        ...Object.fromEntries(
+          Object.entries({
+            ...trackingSearchParams,
+            posthog_person_id: posthogPersonId,
+            fbp,
+            fbc,
+            user_agent: userAgent,
+          }).filter(([_, v]) => v !== undefined))
       })
       .select();
 
